a1aa0d2df9680bc0180c86efd478b90d14a27d6b
Olexandr Yakubenko <olexandr2000xd@gmail.com>
2025-07-11 01:12:51 +0300

    Add basic reference types and some String methods demo

--- src/main/java/ua/parflare/basics/variables/ReferenceTypesDemo.java
+++ src/main/java/ua/parflare/basics/variables/ReferenceTypesDemo.java
@@ -3,7 +3,28 @@ package main.java.ua.parflare.basics.variables;
 public class ReferenceTypesDemo {
 
     public static void main(String[] args) {
+        // Types
+        printTypes();
 
+
+    }
+
+    private static void printTypes() {
+        String text = "Hello, Java!";
+        Integer number = 100;  // wrapper class
+        Double decimal = 99.99;
+
+        System.out.println("String: " + text);
+        System.out.println("Integer object: " + number);
+        System.out.println("Double object: " + decimal);
+
+        // String methods example
+        System.out.println("Length of text: " + text.length());
+        System.out.println("Upper case: " + text.toUpperCase());
+
+        // Null example
+        String nullString = null;
+        System.out.println("Null string: " + nullString);
     }
 
 }
