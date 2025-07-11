a7685e95e5ff229b9b1ef66e2f0a4f12ca23e712
Olexandr Yakubenko <olexandr2000xd@gmail.com>
2025-07-11 17:14:41 +0300

    Add reference types default values

--- src/main/java/ua/parflare/basics/variables/ReferenceTypesDemo.java
+++ src/main/java/ua/parflare/basics/variables/ReferenceTypesDemo.java
@@ -6,7 +6,45 @@ public class ReferenceTypesDemo {
         // Types
         printTypes();
 
+        // Default values
+        new DefaultValues().printDefaults();
 
+    }
+
+    static class DefaultValues {
+        Byte defByte;
+        Short defShort;
+        Integer defInt;
+        Long defLong;
+        Float defFloat;
+        Double defDouble;
+        Character defChar;
+        Boolean defBoolean;
+        String defString;
+        Example defExample;
+
+        void printDefaults() {
+            System.out.println("\nDefault values of reference type fields: (all null)");
+            System.out.println("Byte: " + defByte);
+            System.out.println("Short: " + defShort);
+            System.out.println("Integer: " + defInt);
+            System.out.println("Long: " + defLong);
+            System.out.println("Float: " + defFloat);
+            System.out.println("Double: " + defDouble);
+            System.out.println("Character: " + defChar);
+            System.out.println("Boolean: " + defBoolean);
+            System.out.println("String: " + defString);
+            System.out.println("Example class: " + defExample);
+
+        }
+    }
+
+    private class Example {
+        int age;
+
+        public Example(int age) {
+            this.age = age;
+        }
     }
 
     private static void printTypes() {
